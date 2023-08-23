6177a72b3cfc160e9cbbd51c3f8b5198dc2b9256
anzarbk <ansarzk4@gmail.com>
2023-08-23 15:48:46 +0530

    benefit page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/navbar"
-// import Home from "@/scenes/home"
+import Home from "@/scenes/home"
+import Benefits from "@/scenes/benefits"
 import { useEffect, useState } from "react"
 import {SelectedPage} from "@/shared/types"
 
@@ -22,7 +23,8 @@ function App() {
     <>
       <div className="app bg-gray-20">
         <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-        {/* <Home setSelectedPage={setSelectedPage}/> */}
+        <Home setSelectedPage={setSelectedPage}/>
+        <Benefits setSelectedPage={setSelectedPage}/>
       </div>
      
     </>
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -3,5 +3,11 @@ export enum SelectedPage  {
     Benefits = "benefits",
     OurClasses = "ourclasses",
     ContactUs = "contactus"     
-  }
+}
+  
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
   
